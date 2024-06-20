c05d450e097cddd7ff0670818e9fc8ffcad24ee7
tyler <tyle2003@gmail.com>
2024-06-20 21:57:17 +0900

    made more changes

--- prompt_generation.py
+++ prompt_generation.py
@@ -22,7 +22,7 @@ class caption_generation():
             }
 
         self.bad_answers = [
-            'I don\'t know', 'unknown', 'mystery'
+            'i don\'t know', 'unknown', 'mystery'
             ]
         
         self.blip_questions = {
@@ -86,7 +86,7 @@ class caption_generation():
         answers = []
 
         for question in self.blip_questions:
-            answer = self.generate_one_caption(image, question, max=30).lower()
+            answer = self.generate_one_caption(image, question, max=15).lower()
 
             for bad_ans in self.bad_answers:
                 if bad_ans in answer:
