d77adf6f5e3205a390b30b0a93055380e7936672
tle19 <tyle2003@gmail.com>
2024-06-20 17:47:47 +0900

    base without parallel

--- image_generation.py
+++ image_generation.py
@@ -1,18 +1,12 @@
 import os
 import torch
 from diffusers import StableDiffusionPipeline
-from torch.nn.parallel import DataParallel
 
 class image_generation():
 
     def __init__(self, model_id):
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
         self.pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16, safety_checker = None, requires_safety_checker = False)
-        
-        if torch.cuda.device_count() > 1:
-            print("Using", torch.cuda.device_count(), "GPUs")
-            self.pipe = DataParallel(self.pipe)
-
         self.pipe = self.pipe.to(self.device)
 
     def generate_images(self, names, prompts, sd_folder_path1):
@@ -22,8 +16,8 @@ class image_generation():
         for prompt in prompts:
             print('IMAGE', counter, '-', names[start_val])
             
-            with torch.no_grad():
-                image = self.pipe(prompt).images[0]
+            image = self.pipe(prompt).images[0]
+
             image_path = os.path.join(sd_folder_path1, names[start_val] + '.png')
             image.save(image_path)
 
