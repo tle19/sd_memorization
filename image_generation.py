4d8a1ce256cc799b7edab5828ba563b4e434d7b8
tle19 <tyle2003@gmail.com>
2024-06-20 17:28:08 +0900

    trying dataparallel instead of accelerator

--- image_generation.py
+++ image_generation.py
@@ -1,14 +1,18 @@
 import os
 import torch
 from diffusers import StableDiffusionPipeline
-from accelerate import Accelerator
+from torch.nn.parallel import DataParallel
 
 class image_generation():
 
     def __init__(self, model_id):
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
-        self.device =  Accelerator().device if torch.cuda.device_count() > 1 else self.device
         self.pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16, safety_checker = None, requires_safety_checker = False)
+        
+        if torch.cuda.device_count() > 1:
+            print("Using", torch.cuda.device_count(), "GPUs")
+            self.pipe = DataParallel(self.pipeline)
+
         self.pipe = self.pipe.to(self.device)
 
     def generate_images(self, names, prompts, sd_folder_path1):
@@ -18,7 +22,8 @@ class image_generation():
         for prompt in prompts:
             print('IMAGE', counter, '-', names[start_val])
             
-            image = self.pipe(prompt).images[0]
+            with torch.no_grad():
+                image = self.pipe(prompt).images[0]
             image_path = os.path.join(sd_folder_path1, names[start_val] + '.png')
             image.save(image_path)
 
