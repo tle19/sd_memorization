d9660a74d2645584bdcaa66e0e25d5580afa508b
tyler <tyle2003@gmail.com>
2024-06-23 16:20:02 +0900

    changed back to ethnicity then age

--- caption_generation.py
+++ caption_generation.py
@@ -75,14 +75,14 @@ class CaptionGeneration():
                 answers.append(answer)
 
             hair_and_eyes = f'with {answers[0]} hair and {answers[1]} eyes'
-            # ethnicity = answers[2]
-            # age = f'{answers[3]} year old'
-            age_and_ethnicity = f'{answers[3]} year old {answers[2]}'
+            ethnicity = answers[2]
+            age = f'{answers[3]} year old'
+            # age_and_ethnicity = f'{answers[3]} year old {answers[2]}'
             
             text = self.add_attribute(text, hair_and_eyes, True)
-            text = self.add_attribute(text, age_and_ethnicity)
-            # text = self.add_attribute(text, ethnicity)
-            # text = self.add_attribute(text, age)
+            # text = self.add_attribute(text, age_and_ethnicity)
+            text = self.add_attribute(text, ethnicity)
+            text = self.add_attribute(text, age)
 
             generated_captions.append(text)
 
