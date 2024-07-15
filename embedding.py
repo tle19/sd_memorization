637511e57fdd31b9e5374f8488320298c6b4c630
tyler <tyle2003@gmail.com>
2024-07-16 00:18:13 +0900

    changed dino embedding size

--- embedding.py
+++ embedding.py
@@ -38,7 +38,7 @@ class DINOEmbed(Embed):
         self.processor = AutoImageProcessor.from_pretrained('facebook/dinov2-base')
     
     def process_image(self, image):
-        image = image.convert("RGB").resize((522, 522))
+        image = image.convert("RGB").resize((512, 512))
         return image
 
     def compute_image_feature(self, inputs):
